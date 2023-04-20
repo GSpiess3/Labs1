7def3245f676c4aca91aa55a2448573328d38944
GSpiess3 <gabespiess@gmail.com>
2023-04-20 13:50:31 -0500

    Added second file

---
+++ test_account.py
@@ -0,0 +1,14 @@
+from pytest import *
+from account import *
+
+class Test:
+    def setup_method(self):
+        pass
+    def teardown_method(self):
+        pass
+    def test_init(self):
+        pass
+    def test_deposite(self):
+        pass
+    def test_withdraw(self):
+        pass
